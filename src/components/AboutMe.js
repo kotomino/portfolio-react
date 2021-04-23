3ae42a247b30f5249c1678ecd01cb6dece456789
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-23 17:46:42 -0400

    home page components

---
+++ src/components/AboutMe.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+    <div>
+      AboutMe Component
+    </div>
+  )
+}
+
+export default AboutMe
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import AboutMe from './AboutMe';
+import Contact from './Contact';
+import HomeBanner from './HomeBanner';
+import Projects from './Projects';
+
 
 const Home = () => {
   return (
     <div>
-      Home Page
+      <HomeBanner/>
+      <AboutMe />
+      <Projects />
+      <Contact />
     </div>
   )
 }
