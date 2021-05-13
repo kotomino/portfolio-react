303c504d2b3fb9a221d08912783b3acaeb9ccfdf
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-05-12 20:00:32 -0400

    footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
-import { Button, Container, Grid, Typography } from '@material-ui/core'
 import React from 'react'
+import { Button, Container, Grid, IconButton, Typography } from '@material-ui/core'
+import { Link } from "react-scroll";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = (theme) => ({
@@ -9,13 +15,30 @@ const styles = (theme) => ({
     overflow: 'hidden',
   },
   container: {
-    marginTop: theme.spacing(10),
+    marginTop: theme.spacing(5),
     marginBottom: theme.spacing(10),
     position: 'relative',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
   },
+  middle: {
+    marginTop: 100
+  },
+  // right: {
+  //   marginTop: 30
+  // },
+  linkItem: {
+    '&:hover': {
+      color: 'white',
+      cursor: 'pointer'
+   }
+  },
+  iconItem: {
+    "&:hover": {
+        color: 'white',
+    }
+},
 })
 
 const Footer = (props) => {
@@ -25,9 +48,76 @@ const Footer = (props) => {
   return (
     <section className={classes.root}>
       <Container className={classes.container}>
-        <Typography color="primary">
-         Footer
-        </Typography>
+        <Grid container>
+          <Grid item xs={4} align="center">
+            <div className={classes.left}>
+              <Typography color="textSecondary" variant="h6">
+                GET IN TOUCH
+              </Typography>
+              <Typography color="primary">
+                kotomi.noguchi@gmail.com
+              </Typography>
+              <IconButton aria-label="github link">
+                <GitHubIcon color="primary" className={classes.iconItem} />
+              </IconButton>
+              <IconButton aria-label="github link">
+                <LinkedInIcon color="primary" className={classes.iconItem}/>
+              </IconButton>
+              <IconButton aria-label="website">
+                <InstagramIcon color="primary" className={classes.iconItem}/>
+              </IconButton>
+              <IconButton aria-label="youtube">
+                <YouTubeIcon color="primary" className={classes.iconItem}/>
+               </IconButton>
+               <IconButton aria-label="youtube">
+                <EmailIcon color="primary" className={classes.iconItem}/>
+               </IconButton>
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <div className={classes.middle} align="center">
+              <Typography color="primary">
+                Designed and Built by Kotomi Noguchi
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={4} align="center">
+            <div className={classes.right}>
+              <Typography color="textSecondary" variant="h6">
+                LINKS
+              </Typography>
+              <Link to="about" smooth={true} duration={1200}>
+                <Typography color="primary" display="inline" className={classes.linkItem}>
+                  About{' '}
+                </Typography>
+              </Link>
+              <Typography color="primary" display="inline" >
+                |{' '}
+              </Typography>
+              <Link to="projects" smooth={true} duration={1200}>
+                <Typography color="primary" display="inline" className={classes.linkItem}>
+                  Projects{' '}
+                </Typography>
+              </Link>
+              <Typography color="primary" display="inline" >
+                |{' '}
+              </Typography>
+              <Link to="blogs" smooth={true} duration={1200}>
+                <Typography color="primary" display="inline" className={classes.linkItem}>
+                  Blogs{' '}
+                </Typography>
+              </Link>
+              <Typography color="primary" display="inline" >
+                |{' '}
+              </Typography>
+              <Link to="contact" smooth={true} duration={600}>
+                <Typography color="primary" display="inline" className={classes.linkItem}>
+                  Contact
+                </Typography>
+              </Link>
+            </div>
+          </Grid>
+        </Grid>
       </Container>
     </section>
   )
