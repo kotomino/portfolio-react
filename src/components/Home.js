ec8e59a12f7dc016462186c32981c1e1602bda79
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-05-17 14:20:06 -0500

    removed divider line import

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import AboutMe from './AboutMe';
 import Blogs from './BlogsSection';
 import Contact from './ContactSection';
-import DividerLine from './DividerLine';
 import HomeBanner from './HomeBannerSection';
 import Projects from './ProjectsSection';
 
