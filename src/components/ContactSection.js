954e49773d28921627b793190a13d678641b7d61
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-05-17 14:19:15 -0500

    controlled forms

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Container, Grid, TextField, Typography } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles';
 import { useForm, ValidationError } from '@formspree/react';
@@ -44,6 +44,10 @@ const styles = (theme) => ({
     '&:hover': {
       color: '#248f8f',
     }
+  },
+  thanks: {
+    marginTop: 40,
+    color: "#d1d1e0"
   }
 })
 
@@ -51,10 +55,53 @@ const Contact = (props) => {
   const { classes } = props;
 
   const [state, handleSubmit] = useForm("xknkonle");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleChange = (e) => {
+    switch(e.target.name) {
+      case "name":
+        setName(e.target.value);
+        break;
+      case "email":
+        setEmail(e.target.value);
+        break;
+      case "message":
+        setMessage(e.target.value);
+        break;
+
+    }
+      
+    
+    
+    
+  }
 
   useEffect(() => {
     Aos.init({ duration: 1000 });
   }, [])
+
+  if (state.succeeded) {
+      return (
+      <section className={classes.root} id="contact">
+        <Container className={classes.container}>
+          <Typography variant="h5" color="secondary" display="inline">
+            04.{' '} 
+          </Typography>
+          <Typography variant="h5" className={classes.white} display="inline">
+            What's Next?
+          </Typography>
+          <Typography variant="h3" color="textSecondary">
+            Get In Touch
+          </Typography>
+          <Typography className={classes.thanks}>
+            Thanks for reaching out! I'll try to get back to you ASAP.
+          </Typography>
+        </Container>
+      </section>
+      )
+  }
 
   return (
     <section className={classes.root} id="contact">
@@ -71,34 +118,38 @@ const Contact = (props) => {
           Get In Touch
         </Typography>
         <Grid item md={5} sm={7} xs={11}>
-          <Typography color="primary">
+          <Typography color="primary" className={classes.thanks}>
             My inbox is always open for new opportunities and connections. Feel free to reach out any time!
           </Typography>
         </Grid>
         <form className={classes.form} onSubmit={handleSubmit}>
-          <Grid container spacing={3} align="center">
+          < Grid container spacing={3} align="center">
             <Grid item xs={12}>
-                <TextField
-                  id="name"
-                  label="Your Name"
-                  name="name"
-                  fullWidth
-                  variant="filled" 
-                  InputProps={{
-                    className: classes.textField
-                  }}
-                />
-                <ValidationError 
-                  prefix="Name" 
-                  field="name"
-                  errors={state.errors}
-                />
+              <TextField
+                id="name"
+                label="Your Name"
+                name="name"
+                value={name}
+                onChange={handleChange}
+                fullWidth
+                variant="filled" 
+                InputProps={{
+                  className: classes.textField
+                }}
+              />
+              <ValidationError 
+                prefix="Name" 
+                field="name"
+                errors={state.errors}
+              />
               </Grid>
             <Grid item xs={12}>
               <TextField
                 id="email"
                 label="Your Email"
                 name="email"
+                value={email}
+                onChange={handleChange}
                 fullWidth
                 variant="filled"
                 InputProps={{
@@ -111,15 +162,18 @@ const Contact = (props) => {
                 errors={state.errors}
               />
             </Grid>
-            <Grid item item xs={12}>
+            <Grid item xs={12}>
               <TextField
                 id="message"
+                name="message"
                 variant="filled"
                 label="Your Message"
+                value={message}
+                onChange={handleChange}
                 multiline
                 fullWidth
                 rows={8}
-                defaultValue="Type your message here..."
+                placeholder="Type your message here..."
                 InputProps={{
                   className: classes.textField
                 }}
@@ -130,11 +184,12 @@ const Contact = (props) => {
                 errors={state.errors}
               />
             </Grid>
-            <Grid item item xs={12}>
+            <Grid item xs={12}>
               <Button 
                 type="submit" 
                 variant="outlined" 
                 color="secondary" 
+                disabled={state.submitting}
                 className={classes.button}
                 >Submit
               </Button>
